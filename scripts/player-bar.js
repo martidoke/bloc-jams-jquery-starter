f314e95ca2932e3d0f4194d90e1fa72bd0ab8569
Marti Doke <doke.marti@gmail.com>
2019-03-14 17:04:01 -0600

    sliders_checkpoint#1

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -26,5 +26,23 @@
       player.playPause(previousSong);
     });
 
-  
+    $('#time-control input').on('input', function (event) {
+      player.skipTo(event.target.value);
+   });
+
+    setInterval( () =>
+      if (player.playState !== 'playing') { return; }
+      const currentTime = player.getTime();
+      const duration = player.getDuration();
+      const percent = (currentTime / duration) * 100;
+      $('#time-control .current-time').text( currentTime );
+      $('#time-control input').val(percent);
+      }, 1000);
+
+      $('#volume-control input').on('input', function(event){
+     player.setVolume(event.target.value);
+});
+
+
+
  }
