787e806ae061e2c2ae708ca910ecc018e079d4b3
Marti Doke <doke.marti@gmail.com>
2019-03-14 16:52:39 -0600

    Update player-bar.js #2

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -19,10 +19,12 @@
       if (player.playState !== 'playing') { return; }
 
       const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-      const nextSongIndex = currentSongIndex - 1;
-      if (nextSongIndex >= album.songs.length) { return; }
+      const previousSongIndex = currentSongIndex - 1;
+      if (previousSongIndex === -1) { return; }
 
-      const nextSong = album.songs[nextSongIndex];
-      player.playPause(nextSong);
+      const previousSong = album.songs[previousSongIndex];
+      player.playPause(previousSong);
     });
+
+  
  }
